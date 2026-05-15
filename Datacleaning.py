6059b9745563b854e9c97872b403705fd02c9e52
kakinadasomaraju7-hub <kakinadasomaraju7@gmail.com>
2026-05-15 12:16:00 +0530

    upload the project files

--- Datacleaning.py
+++ Datacleaning.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv("dataset/archive.csv")
+
+
+
+
+df = pd.read_csv(r"C:\Users\VARALAKSHMI KAKINADA\Desktop\VARA LAKSHMI\dataset.csv")
 df.dropna(inplace=True)
 df.drop_duplicates(inplace=True)
 
-df.to_csv("dataset/cleaned_data.csv", index=False)
+
+df.to_csv(r"C:\Users\VARALAKSHMI KAKINADA\Desktop\VARA LAKSHMI\dataset.csv", index=False)
--- Forecasting.py
+++ Forecasting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv(r"C:\Users\VARALAKSHMI KAKINADA\Downloads\archive.zip")
+df = pd.read_csv(r"C:\Users\VARALAKSHMI KAKINADA\Desktop\VARA LAKSHMI\dataset.csv")
 
 df['Order Date'] = pd.to_datetime(df['Order Date'])
 df['Month'] = df['Order Date'].dt.month
